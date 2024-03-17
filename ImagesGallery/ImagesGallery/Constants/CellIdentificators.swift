47a794165c03214f8936190b67775b186321d27b
matsa_007 <serg.arbitran@gmail.com>
2024-03-17 22:01:02 +0300

    feat (ImagesGalleryViewController): Set UI elements for ImagesGalleryViewController Setuped UI elements for ImagesGalleryViewController
    Task: #007

---
+++ ImagesGallery/ImagesGallery/Constants/CellIdentificators.swift
@@ -0,0 +1,12 @@
+//
+//  CellIdentificators.swift
+//  ImagesGallery
+//
+//  Created by Сергей Матвеенко on 17.03.24.
+//
+
+import Foundation
+
+enum CellIdentificators {
+    static let imagesGalleryCellIdentificator = "images_gallery_cell"
+}
---
+++ ImagesGallery/ImagesGallery/Constants/Sizes.swift
@@ -0,0 +1,12 @@
+//
+//  Sizes.swift
+//  ImagesGallery
+//
+//  Created by Сергей Матвеенко on 17.03.24.
+//
+
+import Foundation
+
+enum Sizes {
+    static let imagesGalleryCollectionWidthCoeff: CGFloat = 34 / 35
+}
