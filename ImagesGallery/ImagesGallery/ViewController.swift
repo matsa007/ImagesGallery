444afd9ff33c92f7759f5750a75e04a8c2d2c6ac
matsa_007 <serg.arbitran@gmail.com>
2024-03-17 13:29:56 +0300

    Initial commit

---
+++ ImagesGallery/ImagesGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImagesGallery
+//
+//  Created by Сергей Матвеенко on 17.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
