effe5ff53b88b3bd57277beee56bdf46af845cbb
matsa_007 <serg.arbitran@gmail.com>
2024-03-17 13:58:57 +0300

    feat (app): add FavoriteImagesViewController and ImagesListViewController Added FavoriteImagesViewController Added ImagesListViewController
    Task: #002

---
+++ ImagesGallery/ImagesGallery/FavoriteImages/FavoriteImagesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FavoriteImagesViewController.swift
+//  ImagesGallery
+//
+//  Created by Сергей Матвеенко on 17.03.24.
+//
+
+import UIKit
+
+final class FavoriteImagesViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .green
+    }
+}
