a72496ec57cd7a7fa13505e316bd01d3c6407ed8
matsa_007 <serg.arbitran@gmail.com>
2024-03-19 21:25:19 +0300

    feat (ImagesGalleryCollectionViewCell): Create images gallery collection view custom cell Created custom cell for images gallery collection view
    Task: #018

--- ImagesGallery/ImagesGallery/ImagesGallery/ImagesGalleryViews/ImagesGalleryCollectionViewCell.swift
+++ ImagesGallery/ImagesGallery/ImagesGallery/ImagesGalleryViews/ImagesGalleryCollectionViewCell.swift
@@ -7,4 +7,52 @@
 
 import UIKit
 
-final class ImagesGalleryCollectionViewCell: UICollectionViewCell {}
+final class ImagesGalleryCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - GUI
+
+    private lazy var thumbImageView: UIImageView = {
+        let imView = UIImageView()
+        return imView
+    }()
+    
+    // MARK: - Lifecycle
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        self.thumbImageView.image = nil
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.addSubViews()
+        self.setCellViews()
+        self.setConstraints()
+    }
+    
+    // MARK: - Add subviews
+
+    private func addSubViews() {
+        self.contentView.addSubview(self.thumbImageView)
+    }
+    
+    // MARK: - Layout
+    
+    private func setCellViews() {
+        self.setCell()
+    }
+    
+    private func setCell() {
+        self.contentView.backgroundColor = .blue
+    }
+    
+    // MARK: - Constraints
+    
+    private func setConstraints() {
+        self.thumbImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
