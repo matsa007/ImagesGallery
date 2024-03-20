638610dd3339eccd1ee2f6a2874fc97d62d23a2b
matsa_007 <serg.arbitran@gmail.com>
2024-03-20 17:05:28 +0300

    feat (app): Add network errors handling Added alert for network errors handling
    Task: #023

---
+++ ImagesGallery/ImagesGallery/Constants/ErrorTitles.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorTitles.swift
+//  ImagesGallery
+//
+//  Created by Сергей Матвеенко on 20.03.24.
+//
+
+import Foundation
+
+enum ErrorTitles: String {
+    case alertTitle = "Error"
+    case alertButtonTitle = "Ok"
+}
