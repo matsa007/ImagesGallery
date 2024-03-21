c36af99dc54a93c1306dd16ff1254a4d55984fd9
matsa_007 <serg.arbitran@gmail.com>
2024-03-21 23:32:25 +0300

    features (app): Add detail image display data and it's model Added DetailImageDisplayModel Added detailImageDisplayData to DetailImageViewModel
    Task: #038

---
+++ ImagesGallery/ImagesGallery/DetailImage/DetailImageModels/DetailImageDisplayModel.swift
@@ -0,0 +1,14 @@
+//
+//  DetailImageDisplayModel.swift
+//  ImagesGallery
+//
+//  Created by Сергей Матвеенко on 21.03.24.
+//
+
+import Foundation
+
+struct DetailImageDisplayModel {
+    let currentImageData: Data
+    let currentImageTitle: String?
+    let currentImageDescription: String?
+}
