566d713b7468d27e0da798bf27e9a58aac3f5018
matsa_007 <serg.arbitran@gmail.com>
2024-03-29 01:54:19 +0300

    feat (FavoritesCollectionViewCell): Create favorites collection view cell Created and setuped FavoritesCollectionViewCell
    Task: #069

--- ImagesGallery/ImagesGallery/FavoriteGallery/FavoritesGalleryViews/FavoritesTableViewCell.swift
+++ ImagesGallery/ImagesGallery/FavoriteGallery/FavoritesGalleryViews/FavoritesTableViewCell.swift
@@ -7,4 +7,64 @@
 
 import UIKit
 
-final class FavoritesTableViewCell: UICollectionViewCell {}
+final class FavoritesCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - GUI
+
+    private lazy var favoriteImageView: UIImageView = {
+        let imView = UIImageView()
+        return imView
+    }()
+    
+    // MARK: - Lifecycle
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        self.favoriteImageView.image = nil
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.setupLayout()
+    }
+}
+
+// MARK: - Layout
+
+private extension FavoritesCollectionViewCell {
+    func setupLayout() {
+        self.addSubViews()
+        self.setConstraints()
+    }
+}
+
+// MARK: - Add subviews
+
+private extension FavoritesCollectionViewCell {
+    func addSubViews() {
+        self.contentView.addSubview(self.favoriteImageView)
+    }
+}
+
+// MARK: - Constraints
+
+private extension FavoritesCollectionViewCell {
+    private func setConstraints() {
+        self.favoriteImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - Set cell display data
+
+extension FavoritesCollectionViewCell {
+    func setCellDisplayData(for imageInfo: FavoritesGallerDisplayModel) {
+        let image = UIImage(
+            data: imageInfo.imageData
+        )
+        self.favoriteImageView.image = image
+    }
+}
