c4e06e3edd53bfde6a5f64278bf3039d0617bbde
matsa_007 <serg.arbitran@gmail.com>
2024-03-29 05:25:39 +0300

    feat (FullScreenFavoriteDisplayModel): Add display model for full screen Added FullScreenFavoriteDisplayModel
    Task: #075

---
+++ ImagesGallery/ImagesGallery/FullScreenFavorite/FullScreenFavoriteModel/FullScreenFavoriteDisplayModel.swift
@@ -0,0 +1,13 @@
+//
+//  FullScreenFavoriteDisplayModel.swift
+//  ImagesGallery
+//
+//  Created by Сергей Матвеенко on 29.03.24.
+//
+
+import Foundation
+
+struct FullScreenFavoriteDisplayModel {
+    let imageData: Data
+    let favoritesIndex: Int
+}
